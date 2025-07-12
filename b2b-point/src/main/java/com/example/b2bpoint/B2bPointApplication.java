b1510a284a135588f79c8103608292ba0302e33f
user <user@201-40-161-101.smace300.ipd.brasiltelecom.net.br>
2025-07-12 15:30:51 +0900

    initial commit

---
+++ b2b-point/src/main/java/com/example/b2bpoint/B2bPointApplication.java
@@ -0,0 +1,13 @@
+package com.example.b2bpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2bPointApplication.class, args);
+    }
+
+}
---
+++ b2b-point/src/test/java/com/example/b2bpoint/B2bPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b2bpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
