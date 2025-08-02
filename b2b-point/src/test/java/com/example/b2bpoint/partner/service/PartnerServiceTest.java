c21c2d331467f9a8c6a6a51883704471f979c3e2
Do1K <141199209+Do1K@users.noreply.github.com>
2025-08-02 14:12:34 +0900

    feat: 파트너사 등록 api 추가 & 테스트 코드 작성 (#9)
    * feat: 파트너 등록 기능
    
    * test: Partner등록 service 테스트
    
    * test: Partner등록 Controller 테스트

---
+++ b2b-point/src/test/java/com/example/b2bpoint/partner/service/PartnerServiceTest.java
@@ -0,0 +1,74 @@
+package com.example.b2bpoint.partner.service;
+
+import com.example.b2bpoint.common.exception.CustomException;
+import com.example.b2bpoint.partner.domain.Partner;
+import com.example.b2bpoint.partner.dto.PartnerCreateRequest;
+import com.example.b2bpoint.partner.dto.PartnerResponse;
+import com.example.b2bpoint.partner.repository.PartnerRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PartnerServiceTest {
+
+    @InjectMocks
+    private PartnerService partnerService;
+
+    @Mock
+    private PartnerRepository partnerRepository;
+
+    @DisplayName("파트너사 등록 성공")
+    @Test
+    void createPartner_success() {
+        //given
+        PartnerCreateRequest request=new PartnerCreateRequest("A쇼핑몰", "Amall@gmail.com", "02-12-4567");
+
+        given(partnerRepository.findByBusinessNumber(request.getBusinessNumber())).willReturn(Optional.empty());
+
+        Partner fakeSavedPartner = Partner.builder()
+                .name(request.getName())
+                .contactEmail(request.getContactEmail())
+                .businessNumber(request.getBusinessNumber())
+                .build();
+        given(partnerRepository.save(any(Partner.class))).willReturn(fakeSavedPartner);
+
+        //when
+        PartnerResponse response = partnerService.createPartner(request);
+
+        //then
+        assertThat(response.getName()).isEqualTo("A쇼핑몰");
+        assertThat(response.getContactEmail()).isEqualTo("Amall@gmail.com");
+        assertThat(response.getBusinessNumber()).isEqualTo("02-12-4567");
+
+        verify(partnerRepository).save(any(Partner.class));
+    }
+
+    @DisplayName("이미 존재하는 사업자 번호로 등록 시 예외 발생")
+    @Test
+    void createPartner_fail_whenDuplicateBusinessNumber() {
+        //given
+        PartnerCreateRequest request=new PartnerCreateRequest("A쇼핑몰", "Amall@gmail.com", "02-123-4567");
+
+        given(partnerRepository.findByBusinessNumber(request.getBusinessNumber()))
+                .willReturn(Optional.of(Partner.builder().businessNumber("02-12-4567").build()));
+
+        //when&then
+        assertThrows(CustomException.class, () -> {
+            partnerService.createPartner(request);
+        });
+
+    }
+
+}
