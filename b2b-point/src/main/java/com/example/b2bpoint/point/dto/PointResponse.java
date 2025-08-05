25e95827cc87b0c16504e11cbb5cec5f223ceef7
Do1K <3120kang@naver.com>
2025-08-05 16:45:18 +0900

    test: point적립 service 테스트

--- b2b-point/src/main/java/com/example/b2bpoint/point/dto/PointResponse.java
+++ b2b-point/src/main/java/com/example/b2bpoint/point/dto/PointResponse.java
@@ -2,7 +2,9 @@ package com.example.b2bpoint.point.dto;
 
 import com.example.b2bpoint.point.domain.PointWallet;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class PointResponse {
     private final String userId;
     private final int points;
