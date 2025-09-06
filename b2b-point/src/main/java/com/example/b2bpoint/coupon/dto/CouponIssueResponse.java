b75f10673c07df7ba7d77a107e82f8fbc8d51e9e
Do1K <141199209+Do1K@users.noreply.github.com>
2025-09-06 15:50:24 +0900

    쿠폰 발행 dto 버그 수정 (#25)
    * fix: 쿠폰 발행 dto 컴파일 에러 버그 수정

--- b2b-point/src/main/java/com/example/b2bpoint/coupon/dto/CouponIssueResponse.java
+++ b2b-point/src/main/java/com/example/b2bpoint/coupon/dto/CouponIssueResponse.java
@@ -6,22 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class CouponIssueResponse {
-    private final boolean isSuccess;
     private final String message;
-    private final ErrorCode errorCode;
 
     @Builder
     private CouponIssueResponse(String message) {
         this.message = message;
-        this.errorCode = errorCode;
     }
 
-    public static CouponIssueResponse success(String message) {
-        return new CouponIssueResponse(true, message, null);
-    }
-
-    public static CouponIssueResponse fail(ErrorCode errorCode) {
-        return new CouponIssueResponse(false, null, errorCode);
-    }
 
 }
